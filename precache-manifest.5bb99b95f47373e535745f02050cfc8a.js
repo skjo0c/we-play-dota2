3be104a689a0706e4043b5c1c866208575a4ed41
skj <sujan@innovatetech.co>
2020-05-26 11:57:40 +0545

    Updates

---
+++ precache-manifest.5bb99b95f47373e535745f02050cfc8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c979097b9158aae3ed667ed845663d9",
+    "url": "/we-play-dota/index.html"
+  },
+  {
+    "revision": "56c96df1a4532c2bd3d4",
+    "url": "/we-play-dota/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17d740b314ee19e56871",
+    "url": "/we-play-dota/static/js/2.82bdd1b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/we-play-dota/static/js/2.82bdd1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c96df1a4532c2bd3d4",
+    "url": "/we-play-dota/static/js/main.149cdb63.chunk.js"
+  },
+  {
+    "revision": "fc8ed2cb100b57c3c4ec",
+    "url": "/we-play-dota/static/js/runtime-main.7a36c1ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/we-play-dota/static/media/logo.5d5d9eef.svg"
+  }
+]);
