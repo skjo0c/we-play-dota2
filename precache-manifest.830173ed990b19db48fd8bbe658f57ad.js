602b4a251972eab8d2088cca794e9e8427289c25
skj <sujan@innovatetech.co>
2020-05-26 12:06:37 +0545

    Updates

---
+++ precache-manifest.830173ed990b19db48fd8bbe658f57ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d88d3166f187c23e75f3f7fa4a407e3",
+    "url": "/we-play-dota2/index.html"
+  },
+  {
+    "revision": "51d0aa7bc7149fd5f317",
+    "url": "/we-play-dota2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17d740b314ee19e56871",
+    "url": "/we-play-dota2/static/js/2.82bdd1b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/we-play-dota2/static/js/2.82bdd1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d0aa7bc7149fd5f317",
+    "url": "/we-play-dota2/static/js/main.13331ef6.chunk.js"
+  },
+  {
+    "revision": "5e0af3da81d8116a61d5",
+    "url": "/we-play-dota2/static/js/runtime-main.ff75c69a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/we-play-dota2/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/we-play-dota/precache-manifest.5bb99b95f47373e535745f02050cfc8a.js"
+  "/we-play-dota2/precache-manifest.830173ed990b19db48fd8bbe658f57ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/we-play-dota/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/we-play-dota2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
