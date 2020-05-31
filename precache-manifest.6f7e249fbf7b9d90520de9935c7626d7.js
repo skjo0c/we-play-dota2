6c8c3989bfcd9e8d785bc685c270ad6473a17917
skj <sujan@innovatetech.co>
2020-05-31 17:06:24 +0545

    Updates

---
+++ precache-manifest.6f7e249fbf7b9d90520de9935c7626d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5ea6ae94a62f4a47dab067a1be5553",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2ed130d5dbc77df69ba",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "06c9d8ffd2efd16091c2",
+    "url": "./static/css/main.f6276e34.chunk.css"
+  },
+  {
+    "revision": "e2ed130d5dbc77df69ba",
+    "url": "./static/js/2.91e855a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.91e855a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c9d8ffd2efd16091c2",
+    "url": "./static/js/main.741b7169.chunk.js"
+  },
+  {
+    "revision": "8f71c2fc092395b06b40",
+    "url": "./static/js/runtime-main.3b39500d.js"
+  }
+]);
